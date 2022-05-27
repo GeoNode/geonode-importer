9b8063d72a02a2d335910589ce9f8d0ec172b8c0
Mattia Giupponi <mattia.giupponi@gmail.com>
2022-05-27 16:40:20 +0200

    [Fixes #1] create hookable app for the new importer workflow

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+import importer
+
+
+def read_file(path: str):
+    with open(path, "r") as file:
+        return file.read()
+
+
+setup_requires = [
+    "wheel",
+]
+
+setup(
+    name="geonode-importer",
+    version=importer.__version__,
+    url=importer.__url__,
+    description=importer.__doc__,
+    long_description=read_file("README.md"),
+    author=importer.__author__,
+    author_email=importer.__email__,
+    platforms="any",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django :: 3.0",
+        "License :: OSI Approved :: GNU General Public License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=find_packages(),
+    package_data={"importer": ["templates/*.html", "templates/layers/*.html"]},
+    include_package_data=True,
+    install_requires=[
+        "django-dynamic-model @ git+https://github.com/mattiagiupponi/django-dynamic-models.git@master#egg=dynamic_models",
+    ],
+)
