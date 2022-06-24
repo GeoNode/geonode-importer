254c4c615c61d498275fd4fafafc2fc5fb63b241
mattiagiupponi <mattia.giupponi@gmail.com>
2022-06-24 11:25:21 +0200

    [Fixes #37] fix build

--- setup.py
+++ setup.py
@@ -32,7 +32,8 @@ setup(
     package_data={"importer": ["templates/*.html", "templates/layers/*.html"]},
     include_package_data=True,
     install_requires=[
-        "geopackage-validator @ git+https://github.com/PDOK/geopackage-validator.git@master#egg=geopackage_validator",
+        "gdal==3.2.2",
+        "geopackage-validator @ git+https://github.com/PDOK/geopackage-validator.git#egg=pdok-geopackage-validator",
         "dynamic_models @ git+https://github.com/mattiagiupponi/django-dynamic-models@new_settings#egg=dynamic_models",
     ],
 )
