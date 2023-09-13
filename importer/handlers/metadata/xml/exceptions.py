0ef65133d3d73d242a076acd25af204d48d39a01
mattiagiupponi <mattia.giupponi@gmail.com>
2023-09-13 12:19:02 +0200

    [Fixes #200] Reorganize xml folders

--- importer/handlers/metadata/xml/exceptions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.exceptions import APIException
-from rest_framework import status
-
-
-class InvalidXmlException(APIException):
-    status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = "The xml provided provided is invalid"
-    default_code = "invalid_xml"
-    category = "importer"
