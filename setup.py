20e381f92dd7fe874539ee05ba17d77e062d71d2
mattiagiupponi <mattia.giupponi@gmail.com>
2024-01-11 11:37:55 +0100

    Upgrade to django 4.2

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "setuptools>=59",
+        "gdal<=3.4.3",
         "pdok-geopackage-validator==0.8.5",
         "geonode-django-dynamic-model==0.4.0",
     ],
