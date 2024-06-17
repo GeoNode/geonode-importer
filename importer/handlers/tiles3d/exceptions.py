253581b6cd52d91210e2285bc116c034920b3a89
Mattia <mattia.giupponi@gmail.com>
2024-06-17 18:01:59 +0200

    [Fixes #247] Introducing assets in geonode-importer

--- importer/handlers/tiles3d/exceptions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.exceptions import APIException
-from rest_framework import status
-
-
-class Invalid3DTilesException(APIException):
-    status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = "The 3dtiles provided is invalid"
-    default_code = "invalid_3dtiles"
-    category = "importer"
--- importer/tests/fixture/3dtilesample/README.md
+++
@@ -1,2 +0,0 @@
-Example provided by CensiumGS
-https://github.com/CesiumGS/3d-tiles-samples
